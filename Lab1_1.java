d0e4b8e1f5af6ae5885a4e36615d4d9fc143c841
YutaUWS <113057001+YutaUWS@users.noreply.github.com>
2022-09-13 12:07:36 -0500

    Create Lab1_1.java

---
+++ Lab1_1.java
@@ -0,0 +1,7 @@
+public class Lab1_1 {
+    public static void main(String[] arg){
+        System.out.println("Welcome to CSCI 201!\nThis is (maybe!) my first Java program!\nI'm a \"programmer\"!");
+        System.out.println("   J    A   V     V   A\n   J   A A   V   V   A A\nJ  J  AAAAA   V V   AAAAA");
+        System.out.print(" JJ  A     A   V   A     A");
+    }
+}
